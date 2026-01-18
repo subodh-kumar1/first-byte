af30a4efbe90ae42825450fb3f7f8946f6ea62d6
Subodh Kumar <subodh.subodhkumar1@gmail.com>
2026-01-18 16:12:37 +0530

    Fix base path issue: our application is deployed at https://subodh-kumar1.github.io/first-byte/
    But Next.js by default assumes it lives at /, not /first-byte.
    
    So:
    
    Links go to /lessons instead of So:
    
    Links go to /first-byte/lessons

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: "/first-byte",
+  assetPrefix: "/first-byte/"
+
 };
 
 export default nextConfig;
